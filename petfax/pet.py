9553171cfe721162943607ae93112d4ceffa9a4d
Tudor Comaniciu <tmcomaniciu@gmail.com>
2024-02-23 21:06:16 -0500

    added facts route

--- petfax/pet.py
+++ petfax/pet.py
@@ -6,9 +6,9 @@ pets = json.load(open('pets.json', encoding="UTF-8"))
 
 @bp.route('/')
 def index(): 
-    return render_template('index.html', pets=pets)
+    return render_template('pets/index.html', pets=pets)
 
 @bp.route('/<int:id>')
 def show(id):
     pet = pets[id - 1]
-    return render_template('show.html', pet=pet)
+    return render_template('pets/show.html', pet=pet)
