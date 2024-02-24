33554f05d8e41e051f450457f61d1d27ae2e7d2b
Tudor Comaniciu <tmcomaniciu@gmail.com>
2024-02-23 20:57:22 -0500

    added new routes, new and show

--- petfax/pet.py
+++ petfax/pet.py
@@ -4,8 +4,11 @@ import json
 bp = Blueprint('pet', __name__, url_prefix="/pets")
 pets = json.load(open('pets.json', encoding="UTF-8"))
 
-print(pets)
-
 @bp.route('/')
 def index(): 
     return render_template('index.html', pets=pets)
+
+@bp.route('/<int:id>')
+def show(id):
+    pet = pets[id - 1]
+    return render_template('show.html', pet=pet)
