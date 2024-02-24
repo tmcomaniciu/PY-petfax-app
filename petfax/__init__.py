4b831663aabc669d62db0a2cc975d14ecd41dd41
Tudor Comaniciu <tmcomaniciu@gmail.com>
2024-02-23 22:26:07 -0500

    part five

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,19 +1,22 @@
-from flask import Flask 
+# config                    
+from flask import Flask
 
+# factory
 def create_app():
-    # config
     app = Flask(__name__)
 
     # index route
     @app.route('/')
-    def hello(): 
+    def index(): 
         return 'Hello, PetFax!'
 
-    # register pet blueprint
-    from . import pet
+    # register pet blueprint 
+    from . import pet 
     app.register_blueprint(pet.bp)
 
+    # register fact blueprint 
     from . import fact
     app.register_blueprint(fact.bp)
 
+    # return the app 
     return app
